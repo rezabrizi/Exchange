addd9f3920d0444effa2cbf4759d242ca3131028
Reza Tabrizi <rtabrizi03@gmail.com>
2024-01-02 07:11:11 -0600

    added comments

--- headers/LOB.h
+++ headers/LOB.h
@@ -21,12 +21,12 @@
 
 /**
  * @file LOB.h
- * @brief Singular Limit Order Book for one financial instrument
+ * @brief singular limit order book for one financial instrument
  */
 
 /**
  * @struct ActiveLobMapAndTree
- * @brief Provide pointers to the active binary tree and hashmap for bid or ask orders
+ * @brief provide pointers to the active binary tree and hashmap for bid or ask orders
  */
 struct ActiveLOBMapAndTree{
     std::unordered_map<double, Limit*>* activeMapPtr;
@@ -34,7 +34,8 @@ struct ActiveLOBMapAndTree{
 };
 
 /**
- *
+ * @class LOB
+ * @brief limit order book class for one financial instrument
  */
 class LOB{
     int currentOrderId;
@@ -49,32 +50,164 @@ class LOB{
     std::map <double, Limit*> buyTree;
     std::map <double, Limit*> sellTree;
 
-
+    /**
+     * @brief provide the respective binary tree and hashmap for limit orders
+     * @param bidOrAsk
+     * @return active tree and hashmap (ActiveLOBMapAndTree)
+     */
     ActiveLOBMapAndTree GetActiveMapAndTree(bool bidOrAsk);
+
+    /**
+     * @brief current time with millisecond precision
+     * @return current time (long long)
+     */
     static long long GetTimeStamp ();
+
+    /**
+     * @brief add a market order to the book
+     * @param order order to add
+     */
     void AddMarketOrder(Order* order);
+
+    /**
+     * @brief add a limit order to the book
+     * @param order order to add
+     */
     void AddLimitOrder(Order* order);
+
+    /**
+     * @brief write a new order to the database
+     * @param order order to save to the database
+     */
     void WriteOrderToDB(Order* order);
+
+    /**
+     * @brief write a new execution to the database
+     * @param execution executions to save to the database
+     */
     void WriteExecuteToDB(Execution* execution);
+
+    /**
+     * @brief remove a limit order from the book
+     * @param orderId the id of the order to remove
+     */
     void RemoveLimitOrder(int orderId);
+
+    /**
+     * @brief match market orders
+     * @param executions new executions after the matches are made
+     */
     void MatchMarketOrders(std::vector<Execution*> &executions);
+
+    /**
+     * @brief match limit orders
+     * @param executions new executions after the matches are made
+     */
     void MatchLimitOrders(std::vector<Execution*> &executions);
+
+    /**
+     * @brief walk 2 limit linked lists to find appropriate matches
+     * @param topBidOrder the top appropriate bid
+     * @param topAskOrder the top appropriate ask
+     */
     static void WalkLimits(Order** topBidOrder, Order** topAskOrder);
+
+    /**
+     * @brief walk 1 limit linked list to find appropriate match with a market order
+     * @param topOrder the top appropriate limit order
+     * @param marketOrder the market order to match with the appropriate limit order
+     */
     static void WalkOneLimit(Order** topOrder, Order* marketOrder);
+
+    /**
+     * @brief walk the limit order book to find the appropriate limit price
+     * @param topBidOrder the top appropriate bid
+     * @param topAskOrder the top appropriate ask
+     */
     void WalkBook(Order** topBidOrder, Order** topAskOrder);
+
+    /**
+     * @brief walk one side (bid or ask) of the book to find the appropriate limit price
+     * @param bidOrAsk the side of the book to walk
+     * @param topLimitOrder the top appropriate limit order
+     * @param marketOrder the market order to match with the appropriate limit order
+     */
     void WalkOneBook(bool bidOrAsk, Order** topLimitOrder, Order* marketOrder);
+
+    /**
+     * @param tree the limit tree
+     * @param bestPrice current best price
+     * @return next lower limit (next higher price)
+     */
     static Limit* FindNextLowestLimit(const std::map<double, Limit*>& tree, const double& bestPrice);
+
+    /**
+     * @param tree the limit tree
+     * @param bestPrice current best price
+     * @return next highest limit (next lower price)
+     */
     static Limit* FindNextHighestLimit(const std::map<double, Limit*>& tree, const double& bestPrice);
 
+    void LoadLOBFromDB();
 public:
+
+    /**
+     * @brief LOB constructor
+     */
     LOB(): currentOrderId(0), currentExecutionId(0), bestBid(nullptr), bestAsk(nullptr){};
+
+    /**
+     *
+     * @param instrumentId
+     * @param type
+     * @param clientId
+     * @param bidOrAsk
+     * @param quantity
+     * @param limitPrice
+     * @param entryTime
+     * @return Order Object
+     */
     Order* AddOrder(std::string instrumentId, std::string type, std::string clientId, bool bidOrAsk, int quantity, double limitPrice, long long entryTime);
+
+    /**
+     * @brief execute all possible matches
+     * @param isLimit whether the most recent order was a limit order or not
+     * @return list of executions
+     */
     std::vector<Execution*> Execute(bool isLimit);
+
+    /**
+     * @brief cancelling an individual order
+     * @param orderId the id of the order to cancel
+     * @return the order object that was cancelled
+     */
     Order* CancelOrder(int orderId);
+
+    /**
+     * @brief get the bid volume at a limit price
+     * @param limit
+     * @return volume
+     */
     [[nodiscard]] int GetBidVolumeAtLimit (double limit) const;
+
+    /**
+     * @brief get the ask volume at a limit price
+     * @param limit
+     * @return volume
+     */
     [[nodiscard]] int GetAskVolumeAtLimit (double limit) const;
+
+    /**
+     * @return best bid
+     */
     [[nodiscard]] double GetBestBid () const;
+
+    /**
+     * @return best ask
+     */
     [[nodiscard]] double GetBestAsk () const;
+
+
     void PrintBidBook ();
     void PrintAskBook ();
 };
--- src/LOB.cpp
+++ src/LOB.cpp
@@ -13,8 +13,6 @@ ActiveLOBMapAndTree LOB::GetActiveMapAndTree(bool bidOrAsk){
 
 
 long long LOB::GetTimeStamp() {
-    /
-
     auto now = std::chrono::system_clock::now();
     auto duration = now.time_since_epoch();
     auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
